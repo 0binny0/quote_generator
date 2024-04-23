30666b8f86c0eed9b8f856928724162ff83c3c77
0binny0 <binnyhh@protonmail.com>
2024-04-23 10:54:14 -0700

    added useEffect and ref on App to fetch quotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
+import {useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 import { QuoteGenerator } from "./components/quote_generator/quoteGenerator.jsx";
+import { fetch_quotes } from "./components/quote_generator/utils.js";
 
 function App() {
+
+    const quotes_ref = useRef(null);
+
+    useEffect(() => {
+        (
+            async () => {
+                const quotes = fetch_quotes();
+                quotes_ref.current = quotes;
+            }
+        )()
+    } , []);
+
     return <>
-        <QuoteGenerator quote={} />
+        <QuoteGenerator quote={generate_quote(quotes_ref.current)} />
     </>
 }
 
